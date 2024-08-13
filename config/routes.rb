d389471f23d5c5476cbff77f31fd262203286094
AnamolThapa <anamolthapa7@gmail.com>
2024-08-13 12:04:16 +0900

    added controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  root "galleries#index"
+
+  resources :galleries do
+    resources :applications, only: [:index, :new, :create]
+  end
+
+  resources :applications, only: [:show, :edit, :update, :destroy]
+
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
