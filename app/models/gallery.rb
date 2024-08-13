5c422ee9126ed6d41a7ce85391aee17ad975b123
Joe Estrem <estremjoe@gmail.com>
2024-08-13 18:11:47 +0900

    Made major progress on home page

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -4,7 +4,7 @@ class Gallery < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true, uniqueness: true
-  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :user_id, presence: true
 end
